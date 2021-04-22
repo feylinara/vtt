e3d3ffd5e60d581b5805d8a3682dc9c3965ff4f5
Fey Naomi Schrewe <fey@posteo.eu>
2021-04-22 12:17:45 +0200

    Extract functionality into render lib

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,25 @@ struct HexGridBuilder<'a> {
     outer_radius: u32,
     point_up: bool,
     tiles: &'a [image::DynamicImage],
+    dimensions: (u32, u32),
 }
 
 impl<'a> Default for HexGridBuilder<'a> {
     fn default() -> Self {
         Self {
-            outer_radius: 0, point_up: false, tiles: &[],
+            outer_radius: 0,
+            point_up: false,
+            tiles: &[],
+            dimensions: (0, 0),
         }
     }
 }
 
 impl<'a> HexGridBuilder<'a> {
+    fn with_tiles(mut self, tiles: &'a [image::DynamicImage]) -> Self {
+        self.tiles = tiles;
+        self
+    }
     fn build(self) -> HexGrid {
         let mut texture = render::texture::Texture2D::with_dimensions(
             210 * self.tiles.len() as i32,
@@ -39,10 +47,47 @@ impl<'a> HexGridBuilder<'a> {
             texture.replace_rect(0, 0, image.clone());
         }
 
-        let mut vbo = [0u32; 3];
-        unsafe {
-            gl::GenBuffers(vbo.len() as i32, &mut vbo[0] as *mut GLuint);
-        }
+        let mut vao = render::VertexAttribObject::new();
+
+        let mut vbo: [render::VertexBuffer; 3] = render::VertexBuffer::new_array();
+        vbo[0].data(
+            &QUAD,
+            render::AccessFrequency::Static,
+            render::AccessType::Draw,
+        );
+        vao.vertex_attribute_array(
+            &vbo[0],
+            render::VertexAttribArray::<f32>::with_id(0).with_components_per_value(2),
+        );
+
+        let offsets = grid_coords(4, 4, 210f32);
+        vbo[1].data(
+            &offsets,
+            render::AccessFrequency::Static,
+            render::AccessType::Draw,
+        );
+        vao.vertex_attribute_array(
+            &vbo[1],
+            render::VertexAttribArray::<f32>::with_id(1)
+                .with_components_per_value(2)
+                .with_divisor(6),
+        );
+
+        let tiles = [
+            0f32, 0f32, 1f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32,
+            0f32, 0f32,
+        ];
+        vbo[2].data(
+            &tiles,
+            render::AccessFrequency::Dynamic,
+            render::AccessType::Draw,
+        );
+        vao.vertex_attribute_array(
+            &vbo[2],
+            render::VertexAttribArray::<f32>::with_id(2)
+                .with_components_per_value(2)
+                .with_divisor(6),
+        );
         unimplemented!()
     }
 }
@@ -58,7 +103,6 @@ struct HexGrid {
     vbo: [u32; 3],
     vao: u32,
     texture: render::texture::Texture2D,
-    program: render::program::Program,
 }
 
 const VERT: &str = include_str!("../resources/shaders/grid.vert");
@@ -78,114 +122,109 @@ fn grid_coords(height: u32, width: u32, tile_size: f32) -> Vec<f32> {
 }
 
 fn main() {
-    unsafe {
-        let event_loop = EventLoop::with_user_event();
-        let window = WindowBuilder::new();
-        let context = ContextBuilder::new()
+    let event_loop = EventLoop::with_user_event();
+    let window = WindowBuilder::new();
+    let context = unsafe {
+        ContextBuilder::new()
             .build_windowed(window, &event_loop)
             .unwrap()
             .make_current()
-            .unwrap();
+            .unwrap()
+    };
+    unsafe {
         gl::load_with(|s| context.get_proc_address(s));
         gl::Enable(gl::BLEND);
         gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
-
-        let rt = Runtime::new().unwrap();
-        rt.spawn(other(event_loop.create_proxy()));
-
-        let image1 = image::io::Reader::open(TEST_TILE1)
-            .unwrap()
-            .decode()
-            .unwrap();
-        let image2 = image::io::Reader::open(TEST_TILE2)
-            .unwrap()
-            .decode()
-            .unwrap();
-        let mut texture = render::texture::Texture2D::with_dimensions(
-            210 * 2,
-            210,
-            render::texture::Format::Rgba,
-        );
-        texture.replace_rect(0, 0, image2.clone());
-        texture.replace_rect(210, 0, image1.clone());
-
-        let projection = cgmath::ortho(
-            0f32,
-            context.window().inner_size().width as f32,
-            0f32,
-            context.window().inner_size().height as f32,
-            -1f32,
-            100f32,
-        );
-
-        let program = render::program::ProgramBuilder::default()
-            .attach_shader(
-                render::program::Shader::from_source(render::program::ShaderType::Vertex, VERT)
-                    .unwrap(),
-            )
-            .attach_shader(
-                render::program::Shader::from_source(render::program::ShaderType::Fragment, FRAG)
-                    .unwrap(),
-            )
-            .link()
-            .unwrap();
-
-        let mut vao = 0;
-        unsafe {
-            gl::GenVertexArrays(1, &mut vao as *mut GLuint);
-            gl::BindVertexArray(vao);
-        }
-        let mut vbo = [0; 3];
-        unsafe {
-            gl::GenBuffers(vbo.len() as i32, &mut vbo[0] as *mut GLuint);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo[0]);
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                QUAD.len() as isize * ::std::mem::size_of::<f32>() as isize,
-                &QUAD[0] as *const _ as *const c_void,
-                gl::STATIC_DRAW,
-            );
-        }
-        gl::EnableVertexAttribArray(0);
-        gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, 0, ::std::ptr::null());
-
-        let offsets = grid_coords(4, 4, 210f32);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo[1]);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            offsets.len() as isize * ::std::mem::size_of::<f32>() as isize,
-            &offsets[0] as *const _ as *const c_void,
-            gl::STATIC_DRAW,
-        );
-        gl::EnableVertexAttribArray(1);
-        gl::VertexAttribDivisor(1, 6);
-        gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, 0, ::std::ptr::null());
-
-        let tiles = [
-            0f32, 0f32, 1f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32,
-            0f32, 0f32,
-        ];
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo[2]);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            tiles.len() as isize * ::std::mem::size_of::<f32>() as isize,
-            &tiles[0] as *const _ as *const c_void,
-            gl::STATIC_DRAW,
-        );
-        gl::EnableVertexAttribArray(2);
-        gl::VertexAttribDivisor(2, 6);
-        gl::VertexAttribPointer(2, 1, gl::FLOAT, gl::FALSE, 0, ::std::ptr::null());
-
-        event_loop.run(move |event, _, control_flow| {
-            draw(&program, projection, vao, &context);
-        });
     }
+
+    let rt = Runtime::new().unwrap();
+    rt.spawn(other(event_loop.create_proxy()));
+
+    let image1 = image::io::Reader::open(TEST_TILE1)
+        .unwrap()
+        .decode()
+        .unwrap();
+    let image2 = image::io::Reader::open(TEST_TILE2)
+        .unwrap()
+        .decode()
+        .unwrap();
+    let mut texture =
+        render::texture::Texture2D::with_dimensions(210 * 2, 210, render::texture::Format::Rgba);
+    texture.replace_rect(0, 0, image2.clone());
+    texture.replace_rect(210, 0, image1.clone());
+
+    let projection = cgmath::ortho(
+        0f32,
+        context.window().inner_size().width as f32,
+        0f32,
+        context.window().inner_size().height as f32,
+        -1f32,
+        100f32,
+    );
+
+    let program = render::program::ProgramBuilder::default()
+        .attach_shader(
+            render::program::Shader::from_source(render::program::ShaderType::Vertex, VERT)
+                .unwrap(),
+        )
+        .attach_shader(
+            render::program::Shader::from_source(render::program::ShaderType::Fragment, FRAG)
+                .unwrap(),
+        )
+        .link()
+        .unwrap();
+
+    let mut vao = render::VertexAttribObject::new();
+
+    let mut vbo: [render::VertexBuffer; 3] = render::VertexBuffer::new_array();
+    vbo[0].data(
+        &QUAD,
+        render::AccessFrequency::Static,
+        render::AccessType::Draw,
+    );
+    vao.vertex_attribute_array(
+        &vbo[0],
+        render::VertexAttribArray::<f32>::with_id(0).with_components_per_value(2),
+    );
+
+    let offsets = grid_coords(4, 4, 210f32);
+    vbo[1].data(
+        &offsets,
+        render::AccessFrequency::Static,
+        render::AccessType::Draw,
+    );
+    vao.vertex_attribute_array(
+        &vbo[1],
+        render::VertexAttribArray::<f32>::with_id(1)
+            .with_components_per_value(2)
+            .with_divisor(6),
+    );
+
+    let tiles = [
+        0f32, 0f32, 1f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32, 0f32,
+        0f32,
+    ];
+    vbo[2].data(
+        &tiles,
+        render::AccessFrequency::Dynamic,
+        render::AccessType::Draw,
+    );
+    vao.vertex_attribute_array(
+        &vbo[2],
+        render::VertexAttribArray::<f32>::with_id(2)
+            .with_components_per_value(2)
+            .with_divisor(6),
+    );
+
+    event_loop.run(move |event, _, control_flow| unsafe {
+        draw(&program, projection, vao, &context);
+    });
 }
 
 unsafe fn draw(
     program: &render::Program,
     projection: cgmath::Matrix4<f32>,
-    vao: u32,
+    vao: render::VertexAttribObject,
     context: &glutin::ContextWrapper<glutin::PossiblyCurrent, glutin::window::Window>,
 ) {
     gl::ClearColor(0.8, 0.8, 0.8, 1.0);
@@ -194,7 +233,7 @@ unsafe fn draw(
     program.uniform_mat4("projection", &projection);
     program.uniform_vec2("size", [210f32, 210f32].into());
     program.uniform_f32("ntiles", 2.0);
-    gl::BindVertexArray(vao);
+    vao.bind();
     gl::DrawArraysInstanced(gl::TRIANGLES, 0, 6 as i32, 6 * 16);
     context.swap_buffers().unwrap();
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -7,6 +7,12 @@ use gl;
 use gl::types::GLuint;
 use std::os::raw::c_void;
 
+pub trait Bindable {
+    fn bind(&self);
+}
+
+pub trait Buffer: Bindable {}
+
 #[repr(transparent)]
 #[derive(Copy, Clone)]
 pub struct VertexBuffer {
@@ -22,16 +28,162 @@ impl VertexBuffer {
         Self { id }
     }
     pub fn new_array<const N: usize>() -> [Self; N] {
-        let mut ids = [Self {id: 0}; N];
+        let mut ids = [Self { id: 0 }; N];
         unsafe {
             gl::GenBuffers(N as i32, &mut ids[0] as *mut Self as *mut GLuint);
+        }
+        ids
+    }
+    pub fn data<T>(&self, data: &[T], freq: AccessFrequency, typ: AccessType) {
+        self.bind();
+        unsafe {
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                data.len() as isize * ::std::mem::size_of::<T>() as isize,
+                &data[0] as *const _ as *const c_void,
+                storage_type(freq, typ),
+            );
+        }
+    }
+}
+
+impl Bindable for VertexBuffer {
+    fn bind(&self) {
+        unsafe {
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.id);
+        }
+    }
+}
+
+impl Buffer for VertexBuffer {}
+
+#[repr(transparent)]
+#[derive(Copy, Clone)]
+pub struct VertexAttribObject {
+    id: GLuint,
+}
+
+impl VertexAttribObject {
+    pub fn new() -> Self {
+        let mut id = 0;
+        unsafe {
+            gl::GenVertexArrays(1, &mut id as *mut GLuint);
+        }
+        Self { id }
+    }
+    pub fn new_array<const N: usize>() -> [Self; N] {
+        let mut ids = [Self { id: 0 }; N];
+        unsafe {
+            gl::GenVertexArrays(N as i32, &mut ids[0] as *mut Self as *mut GLuint);
         }
         ids
     }
 
     pub fn bind(&self) {
         unsafe {
-        gl::BindBuffer(gl::ARRAY_BUFFER, self.id);
+            gl::BindVertexArray(self.id);
         }
+    }
+
+    pub fn vertex_attribute_array<T: GlType>(&self, buffer: &dyn Buffer, ptr: VertexAttribArray<T>) {
+        self.bind();
+        buffer.bind();
+        unsafe {
+            gl::EnableVertexAttribArray(ptr.id);
+            ptr.divisor
+                .map(|divisor| gl::VertexAttribDivisor(ptr.id, divisor));
+            gl::VertexAttribPointer(
+                ptr.id,
+                ptr.ncomponents,
+                T::to_enum(),
+                if ptr.normalise { gl::TRUE } else { gl::FALSE },
+                ptr.stride,
+                ptr.offset as *mut c_void,
+            );
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum AccessFrequency {
+    Static,
+    Stream,
+    Dynamic,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum AccessType {
+    Draw,
+    Read,
+    Copy,
+}
+
+fn storage_type(freq: AccessFrequency, typ: AccessType) -> GLuint {
+    use AccessFrequency::*;
+    use AccessType::*;
+    match (freq, typ) {
+        (Static, Draw) => gl::STATIC_DRAW,
+        (Static, Read) => gl::STATIC_READ,
+        (Static, Copy) => gl::STATIC_COPY,
+        (Stream, Draw) => gl::STREAM_DRAW,
+        (Stream, Read) => gl::STREAM_READ,
+        (Stream, Copy) => gl::STREAM_COPY,
+        (Dynamic, Draw) => gl::DYNAMIC_DRAW,
+        (Dynamic, Read) => gl::DYNAMIC_READ,
+        (Dynamic, Copy) => gl::DYNAMIC_COPY,
+    }
+}
+
+pub trait GlType {
+    fn to_enum() -> GLuint;
+}
+
+impl GlType for f32 {
+    fn to_enum() -> GLuint {
+        gl::FLOAT
+    }
+}
+
+pub struct VertexAttribArray<T: GlType> {
+    id: u32,
+    divisor: Option<u32>,
+    ncomponents: i32,
+    normalise: bool,
+    stride: i32,
+    offset: i32,
+    _pd: std::marker::PhantomData<T>,
+}
+
+impl<T: GlType> VertexAttribArray<T> {
+    pub fn with_id(id: u32) -> Self {
+        Self {
+            id,
+            divisor: None,
+            normalise: false,
+            stride: 0,
+            offset: 0,
+            ncomponents: 1,
+            _pd: std::marker::PhantomData,
+        }
+    }
+    pub fn normalise(mut self) -> Self {
+        self.normalise = true;
+        self
+    }
+    pub fn with_divisor(mut self, divisor: u32) -> Self {
+        self.divisor = Some(divisor);
+        self
+    }
+    pub fn with_components_per_value(mut self, ncomponents: i32) -> Self {
+        self.ncomponents = ncomponents;
+        self
+    }
+    pub fn with_stride(mut self, stride: i32) -> Self {
+        self.stride = stride;
+        self
+    }
+    pub fn with_offset(mut self, offset: i32) -> Self {
+        self.offset = offset;
+        self
     }
 }
